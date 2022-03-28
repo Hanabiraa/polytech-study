a5b80f221d3f144c6462d009702a49879e16a256
Hanabiraa <workflow.elec@gmail.com>
2022-03-28 19:06:09 +0300

    feat/add lab2 1B for operating systems

---
+++ operating_system_programming/lab2_mutex/1B/client.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+int main() {
+    return 0;
+}
---
+++ operating_system_programming/lab2_mutex/1B/server.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+int shmget(key_t key, size_t size, int shmflg)
+
+int main()
+{
+
+    return 0;
+}
