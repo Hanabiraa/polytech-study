d826fa49fd01a51025ea3b23c4fd3ebe84a278ad
Vitaly Kopylov <workflow.elec@gmail.com>
2022-04-16 16:34:10 +0100

    feat/add mfc lab 6 oop

---
+++ object_oriented_programming/mfc/MFClab6/MFClab6.h
@@ -0,0 +1,32 @@
+
+// MFClab6.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFClab6App:
+// See MFClab6.cpp for the implementation of this class
+//
+
+class CMFClab6App : public CWinApp
+{
+public:
+	CMFClab6App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFClab6App theApp;
---
+++ object_oriented_programming/mfc/MFClab6/resource.h
@@ -0,0 +1,27 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFClab6.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCLAB6_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_RESULTS_EDIT                1000
+#define IDC_RESULT_STATIC               1001
+#define IDC_YESNOCANCEL_BUTTON          1003
+#define IDC_EXIT_BUTTON                 1004
+#define IDC_YESNO_BUTTON                1005
+#define IDC_OKCANCEL_BUTTON             1006
+#define IDC_RETRYCANCEL_BUTTON          1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
