d55cffddab781dc021d818e5bfc86cd6e6e9f332
Vitaly Kopylov <workflow.elec@gmail.com>
2022-04-28 18:42:03 +0100

    feat/add minor changes

--- computer_architecture/lab3_MPI/main.cpp
+++ computer_architecture/lab3_MPI/main.cpp
@@ -1,13 +1,13 @@
 ﻿#include "tasks.h"
 
 int main(int argc, char** argv) {
-    //cout << "===== TASK 2 =====\n\n";
-    //task2();
-    //cout << "\n===== ====== =====\n";
+    cout << "===== TASK 2 =====\n\n";
+    task2();
+    cout << "\n===== ====== =====\n";
 
-    //cout << "===== TASK 3 =====\n\n";
-    //task3();
-    //cout << "\n===== ====== =====\n";
+    cout << "===== TASK 3 =====\n\n";
+    task3();
+    cout << "\n===== ====== =====\n";
 
     cout << "===== TASK 4 =====\n\n";
     task4();
