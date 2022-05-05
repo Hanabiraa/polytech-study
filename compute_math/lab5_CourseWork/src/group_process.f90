159e1daa14e32ade6a064e83fb4b867b085fdd45
Hanabiraa <workflow.elec@gmail.com>
2022-05-05 23:03:01 +0300

    feat/change CourseWork for compute_math

--- compute_math/lab5_CourseWork/src/group_process.f90
+++ compute_math/lab5_CourseWork/src/group_process.f90
@@ -2,6 +2,25 @@ module Group_Process
     use Environment
 
 contains
+    ! система дифф. уравнений
+    real(R_) function find_dz1(z2)
+        real(R_)    :: z2
+        find_dz1 = z2 * z2 - 1
+    end function find_dz1
+
+    real(R_) function find_dz2(z1)
+        real(R_)    :: z1
+        find_dz2 = z1
+    end function find_dz2
+
+    subroutine system(t, z, dz)
+        real(R_),   intent(inout)   :: t, z(2)
+        real(R_),   intent(inout)   :: dz(2)
+
+        dz(1) = find_dz1(z(2))
+        dz(2) = find_dz2(z(1))
+    end subroutine system
+
     ! подинтегральная функция
     real(R_) function integralFunc(x)
         ! Параметры
--- compute_math/lab5_CourseWork/src/main.f90
+++ compute_math/lab5_CourseWork/src/main.f90
@@ -4,10 +4,17 @@ program lab4_ZEROIN
     use Group_Process
 
 
-    ! Переменные для интегрирования
+    ! Переменные для интегрирования QUANC8
     real(R_)    :: A_integ, B_integ, ABSERR, RELERR, RESULT, ERREST, FLAG
     integer(I_) :: NOFUN=0
+    ! Переменные для ZEROIN
+    real(R_)    :: A, B, ANS, TOL
 
+    ! переменные для RKF45
+    real(R_)                    ::  WORK(15), z(2), dz(2)
+    integer(I_)                 ::  NEQN = 2, IFLAG = +1
+    integer(I_)                 ::  IWORK(5)
+    real(R_)                    ::  T, TOUT
 
     ! Для циклов, ввода-вывода и прочего
     integer(I_)                 :: i = 0, Out = 1
@@ -26,7 +33,74 @@ program lab4_ZEROIN
         format = "(a)"
         write(Out, format) "Для метода пристрелки сперва найдем границу интервала."
         format = "(a, 1f10.5)"
+        result = result * 25
         write(Out, format) "A = ", result
+        write(Out, *) " "
     close(Out)
+
+    ! Вычисления lambda посредством ZEROIN
+    open (file=output_file, newunit=Out, position="append")
+        A = result
+        B = 1.5
+        TOL = 10 ** (-6)
+        ANS = zeroin(A, B, f, TOL)
+
+        format = "(a)"
+        write(Out, format) "функция ZEROIN вычислила lambda."
+        format = "(a, 1f10.5)"
+        write(Out, format) "lambda = ", ANS
+        write(Out, *) " "
+    close(Out)
+
+
+
+    ! Вычисления RKF45 после найденного lambda
+    open (file=output_file, newunit=Out, position="append")
+        RELERR = 1.E-6
+        ABSERR = 1.E-6
+        T = 0.0
+        TOUT = 1.0
+        z(1) = ANS
+        z(2) = 0
+        call RKF45(system, NEQN, z, T, TOUT, RELERR, ABSERR, IFLAG, WORK, IWORK)
+
+        format = "(a)"
+        write(Out, format) "Подставим в RKF45 lambda и проведем вычисления еще раз."
+        format = "(a, 1f10.5)"
+        write(Out, format) "z1 = ", z(1)
+        write(Out, format) "z2 = ", z(2)
+        write(Out, *) " "
+    close(Out)
+
+    ! Невязка
+    open (file=output_file, newunit=Out, position="append")
+        format = "(a, 1f10.5)"
+        write(Out, format) "Невязка равна: ", 1 - z(2)
+    close(Out)
+
 contains
+    ! функция для ZEROIN
+    real(R_) function f(lambda)
+        ! Входные переменные
+        real(R_)    :: lambda
+
+        ! Локальные переменные
+        ! Для RKF45
+        real(R_)                    :: RELERR = 1.E-6, ABSERR = 1.E-6
+        real(R_)                    ::  WORK(15), z(2), dz(2)
+        integer(I_)                 ::  NEQN = 2, IFLAG = +1
+        integer(I_)                 ::  IWORK(5)
+        real(R_)                    ::  T, TOUT
+
+        ! Границы интегрирования
+        T = 0
+        TOUT = 1
+
+        ! Начальные значения
+        z(1) = lambda
+        z(2) = 0
+        call RKF45(system, NEQN, z, T, TOUT, RELERR, ABSERR, IFLAG, WORK, IWORK)
+
+        f = z(2) - 1
+    end function f
 end program lab4_ZEROIN
