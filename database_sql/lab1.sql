dba19cbfc85d5f1793227576f31eb3b740523337
Hanabiraa <workflow.elec@gmail.com>
2022-10-20 19:25:02 +0300

    feat/add lab1

--- database_sql/lab1.sql
+++ database_sql/lab1.sql
@@ -4,123 +4,167 @@ SET search_path TO polytech_lab;
 SHOW search_path;
 
 -- Создание таблиц
--- alter table -> id -> primary key
+CREATE TABLE addresses
+(
+    id      serial PRIMARY KEY,
+    city    text,
+    address text,
+    office  int
+);
+
+CREATE TABLE manufacturers
+(
+    id   serial PRIMARY KEY,
+    name text,
+    address_id     int
+        CONSTRAINT fk1 REFERENCES addresses (id)
+);
+
 CREATE TABLE item
 (
-    name         text PRIMARY KEY,
-    manufacturer text NOT NULL,
-    unit         text NOT NULL
+    id              serial PRIMARY KEY,
+    name            text,
+    manufacturer_id int
+        CONSTRAINT fk1 REFERENCES manufacturers (id),
+    unit            text,
+    price_per_unit  float
 );
 
 CREATE TABLE buyer
 (
-    id              serial PRIMARY KEY,
-    name            text NOT NULL,
-    address         text NOT NULL,
-    phone           text NOT NULL,
-    contanct_person text NOT NULL
+    id             serial PRIMARY KEY,
+    name           text,
+    address_id     int
+        CONSTRAINT fk1 REFERENCES addresses (id),
+    phone          text,
+    contact_person text
 );
 
 CREATE TABLE purchase
 (
     id             serial PRIMARY KEY,
-    quantity       int NOT NULL,
-    price_per_unit float,
-    item_name      text
-        CONSTRAINT fk1 REFERENCES item (name),
+    quantity       int,
+    item_id        int
+        CONSTRAINT fk1 REFERENCES item (id),
     buyer_id       int
         CONSTRAINT fk2 REFERENCES buyer (id),
-    date           timestamptz DEFAULT now()
+    date           timestamp DEFAULT now()
 );
 
 -- вставка значений
-WITH RECURSIVE cte AS (SELECT 0    AS val,
-                              NULL AS name,
-                              NULL as random_num,
-                              NULL AS manufacturer_name,
-                              NULL As unit
-                       UNION ALL
-                       SELECT val + 1,
-                              'paint' || (val + 1)::text,
-                              (floor(random() * 10)::int % 4)::text,
-                              CASE
-                                  WHEN random_num < '0' THEN 'breaks_company'
-                                  WHEN random_num < '1' THEN 'toon_company'
-                                  WHEN random_num < '2' THEN 'big_company'
-                                  ELSE 'sun_company'
-                                  END,
-                              CASE
-                                  WHEN random_num < '0' THEN 'milliliters'
-                                  WHEN random_num < '1' THEN 'liters'
-                                  WHEN random_num < '2' THEN 'cubic centimeter'
-                                  ELSE 'cubic decimeter'
-                                  END
-                       FROM cte
-                       WHERE val < 15)
-INSERT
-INTO item(name, manufacturer, unit)
-SELECT name, manufacturer_name, unit
-FROM cte
-OFFSET 1;
 
-WITH RECURSIVE cte AS (SELECT 0    AS val,
-                              NULL as random_num,
-                              NULL AS name1,
-                              NULL As name2,
-                              NULL As address,
-                              NULL As phone
-                       UNION ALL
+INSERT INTO addresses
+VALUES (DEFAULT, 'Moscow', 'Pushkin street', 1),
+       (DEFAULT, 'Saint Petersburg', 'Lermontov street', 10),
+       (DEFAULT, 'Moscow', 'Motrosova street', 23),
+       (DEFAULT, 'Vologda', 'Pushkin street', 12),
+       (DEFAULT, 'Krasnoyarsk', 'Lermontov street', 14),
+       (DEFAULT, 'Moscow', 'Pushkin street', 6),
+       (DEFAULT, 'Krasnoyarsk', 'Motrosova street', 16),
+       (DEFAULT, 'Krasnoyarsk', 'Pushkin street', 12),
+       (DEFAULT, 'Vologda', 'Motrosova street', 14)
 
-                       SELECT val + 1,
-                              (floor(random() * 10)::int % 4)::text,
-                              CASE
-                                  WHEN random_num < '0' THEN 'Ivan'
-                                  WHEN random_num < '1' THEN 'Petr'
-                                  WHEN random_num < '2' THEN 'Sergei'
-                                  ELSE 'Alexey'
-                                  END,
-                              CASE
-                                  WHEN random_num < '0' THEN 'Ivanovich'
-                                  WHEN random_num < '1' THEN 'Stepanovich'
-                                  WHEN random_num < '2' THEN 'Maksimovich'
-                                  ELSE 'Nikolaevich'
-                                  END,
-                              CASE
-                                  WHEN random_num < '0' THEN 'Space ' || random_num
-                                  WHEN random_num < '1' THEN 'Star ' || random_num
-                                  WHEN random_num < '2' THEN 'Eleven ' || random_num
-                                  ELSE 'Cubic ' || random_num
-                                  END,
-                              floor(random() * 10 ^ 7)::text
-                       FROM cte
-                       WHERE val < 16)
-INSERT
-INTO buyer(name, address, phone, contanct_person)
-SELECT name1 || ' ' || name2, address || ' street', '+7' || phone, name1
-FROM cte
-OFFSET 2;
+INSERT INTO manufacturers
+VALUES (DEFAULT, 'Break company', 1),
+       (DEFAULT, 'Paint manufacture', 2),
+       (DEFAULT, 'Toona industries', 3),
+       (DEFAULT, 'Car factory', 4)
 
-INSERT INTO purchase(quantity, price_per_unit, item_name, buyer_id, date)
-VALUES (floor(random() * 100)::int, random() * 10, 'paint1', 10, default),
-       (floor(random() * 100)::int, random() * 10, 'paint2', 11, default),
-       (floor(random() * 100)::int, random() * 10, 'paint3', 12, default),
-       (floor(random() * 100)::int, random() * 10, 'paint4', 13, default),
-       (floor(random() * 100)::int, random() * 10, 'paint5', 14, default),
-       (floor(random() * 100)::int, random() * 10, 'paint7', 15, default),
-       (floor(random() * 100)::int, random() * 10, 'paint8', 16, default),
-       (floor(random() * 100)::int, random() * 10, 'paint9', 17, default),
-       (floor(random() * 100)::int, random() * 10, 'paint10', 18, default);
+INSERT INTO item
+VALUES (DEFAULT, 'standart break', 1, 'kg', random() * 10),
+       (DEFAULT, 'light break', 1, 'kg', random() * 10),
+       (DEFAULT, 'heavy break', 1, 'kg', random() * 10),
+       (DEFAULT, 'blue paint', 2, 'litters', floor(random() * 10) + 2),
+       (DEFAULT, 'white paint', 2, 'litters', floor(random() * 10) + 2),
+       (DEFAULT, 'red paint', 2, 'litters', floor(random() * 10)+ 2),
+       (DEFAULT, 'small fish', 3, 'kg', random() * 10),
+       (DEFAULT, 'big fish', 3, 'kg', random() * 10),
+       (DEFAULT, 'light car', 4, 'unit', random() * 10000),
+       (DEFAULT, 'sport car', 4, 'unit', random() * 10000),
+       (DEFAULT, 'city car', 4, 'unit', random() * 10000)
+
+INSERT INTO buyer
+VALUES (DEFAULT, 'LTD company', 5, '8999191919', 'Ivan Ivanov'),
+       (DEFAULT, 'CORP manufacture', 6, '8999191919', 'Petr Petrov'),
+       (DEFAULT, 'DOS factory', 7, '8999191919', 'Alexey Kirilovich'),
+       (DEFAULT, 'SCS industry', 8, '8999191919', 'Mark Valerievich'),
+       (DEFAULT, 'Bee company', 9, '8999191919', 'Ivan Petrov')
 
 
--- Запросы
+INSERT INTO purchase
+VALUES (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 day 1 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '2 day 2 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '3 day 3 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '4 day 4 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '5 day 5 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '6 day 6 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 7 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 8 hour'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 8 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 11 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 12 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '1 month 13 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '3 month 8 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '2 month 8 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '4 month 1 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '4 month 2 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '4 month 3 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '4 month 4 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '2 month 5 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, now() - '2 month 6 day'::interval),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT),
+       (DEFAULT, floor(random() * 10 + 2)::int, floor(random() * 10000)::int % 11 + 1, floor(random() * 10000)::int % 5 + 1, DEFAULT);
 
--- -- 1 -Запрос с заказами ТОЛЬКО за прошедший месяц
-SELECT *
+-- выборка покупателей, где они совершили больше 3 но меньше 7 покупок
+-- за последние полтора месяца
+-- c id и именем покупателя, количеством товара и названием товара,
+WITH cte AS (
+    SELECT * FROM purchase WHERE purchase.date > now() - '1 month 15 day'::interval
+)
+SELECT b.id AS id, b.name, sum(p.quantity) AS quantity, i.name AS item
+FROM cte AS p
+         JOIN buyer AS b ON b.id = p.buyer_id
+         JOIN item AS i ON i.id = p.item_id
+WHERE p.quantity BETWEEN 3 AND 7
+GROUP BY b.id, b.name, i.name;
+
+-- Выборка, где покупатели совершили суммарно больше 6 любых покупок
+SELECT b.name, count(p.id) AS amount
 FROM purchase AS p
-         LEFT JOIN item AS i ON p.item_name = i.name
-         LEFT JOIN buyer AS b ON p.buyer_id = b.id
-WHERE date > now() - interval '1 month';
+         JOIN buyer b on p.buyer_id = b.id
+GROUP BY b.name
+HAVING count(*) > 5;
 
+-- Средняя сумма заказа для каждой компании
+WITH cte AS (
+    SELECT p.id AS id, p.quantity * i.price_per_unit AS amount
+    FROM purchase AS p
+         JOIN item AS i ON i.id = p.item_id
+)
+SELECT b.name, avg(cte.amount) AS average_amount
+FROM purchase AS p
+         JOIN buyer AS b ON b.id = p.buyer_id
+         JOIN item AS i ON i.id = p.item_id
+         JOIN cte AS cte ON cte.id = p.id
+GROUP BY b.name;
 
--- группировки, оконные функции,
--- select, где покупатели, которые за какой-то интервал времени (1 день допустим) которые совершили от 2 до 3 покупок
+-- Средняя количество товара каждого вида для каждой компании
+SELECT DISTINCT b.name, i.name, avg(p.quantity) OVER (PARTITION BY b.name, i.name ORDER BY b.name DESC) AS avg
+FROM purchase AS p
+         JOIN buyer AS b ON b.id = p.buyer_id
+         JOIN item AS i ON i.id = p.item_id
+GROUP BY p.id, b.name, i.name
+ORDER BY b.name DESC;
